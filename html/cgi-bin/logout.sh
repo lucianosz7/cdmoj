ff07bc45107d06a90893e144dbd02e884edbafa0
Luciano Santos <luciano_z7@hotmail.com>
2022-03-22 10:03:42 -0300

    Fixing logout

--- html/cgi-bin/logout.sh
+++ html/cgi-bin/logout.sh
@@ -17,9 +17,6 @@
 source common.sh
 AGORA=$(date +%s)
 
-rm -rf $CACHEDIR/*
-echo "#SCRIPTSDIR#/clean-cache.sh" >> "$CACHEDIR/teste"
-#bash #SCRIPTSDIR#/clean-cache.sh
 printf "Set-Cookie: login=$LOGIN; Path=/;  expires=$(date --date=@$AGORA)\n"
 printf "Set-Cookie: hash=0000; Path=/; expires=$(date --date=@$AGORA)\n"
 printf "Content-type: text/html\n\n"
