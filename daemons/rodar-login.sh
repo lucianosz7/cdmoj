9cdbfcdbe749df2797e3fd3162e5df91be3d79b3
Luciano Santos <luciano_z7@hotmail.com>
2022-03-21 22:13:14 -0300

    Testing inotifywait

--- daemons/rodar-login.sh
+++ daemons/rodar-login.sh
@@ -3,22 +3,29 @@
 
 source #CONFDIR#/common.conf
 
-cd $CACHEDIR || return 
+cd $CACHEDIR
 
 while true; do
-	ls -A > "$CACHEDIR/oldFiles"
-	if [[ -e $CACHEDIR/newFiles ]]; then
-		continue
-    else
-		touch "$CACHEDIR/newFiles"
-	fi
-
-	DIRDIFF=$(diff "$CACHEDIR/oldFiles" "$CACHEDIR/newFiles" | cut -f 2 -d "")
-
-    if ! $DIRDIFF; then
+	inotifywait -m $CACHEDIR -e create -e moved_to |
+		while read dir action file; do
+			bash #BASEDIR#/judge/login.sh
+		done
+		
 		printf "."
 		sleep 3
 		continue
-	fi
-    bash #BASEDIR#/judge/login.sh
+	#ls -A > "$CACHEDIR/oldFiles"
+	#if [[ -e $CACHEDIR/newFiles ]]; then
+	#	continue
+    #fi
+	#touch "$CACHEDIR/newFiles"
+
+	#DIRDIFF=$(diff "$CACHEDIR/oldFiles" "$CACHEDIR/newFiles" | cut -f 2 -d "")
+
+    #if diff "$CACHEDIR/oldFiles" "$CACHEDIR/newFiles" | cut -f 2 -d ""; then
+	#	printf "."
+	#	sleep 3
+	#	continue
+	#fi
+    #bash #BASEDIR#/judge/login.sh
 done
