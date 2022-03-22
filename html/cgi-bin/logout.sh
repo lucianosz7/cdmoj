3de4a4b34dbaaadaa4c5f2740d4497e87b51df43
Luciano Santos <luciano_z7@hotmail.com>
2022-03-22 10:53:59 -0300

    Introduce end-of-line normalization

--- html/cgi-bin/logout.sh
+++ html/cgi-bin/logout.sh
@@ -17,6 +17,7 @@
 source common.sh
 AGORA=$(date +%s)
 
+
 printf "Set-Cookie: login=$LOGIN; Path=/;  expires=$(date --date=@$AGORA)\n"
 printf "Set-Cookie: hash=0000; Path=/; expires=$(date --date=@$AGORA)\n"
 printf "Content-type: text/html\n\n"
