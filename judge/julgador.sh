30c41d1c5ae48be16289594c37de77375d9d2f85
Luciano Santos <luciano_z7@hotmail.com>
2022-08-29 22:26:52 -0300

    Adding jplag feature

--- judge/julgador.sh
+++ judge/julgador.sh
@@ -106,6 +106,7 @@ for ARQ in $SUBMISSIONDIR/*; do
   LING="$(cut -d: -f7 <<< "$N")"
   RESP="$(cut -d: -f8 <<< "$N")"
   #LING="$(file $ARQ|awk '{print $3}')"
+  STATUS="$(cut -d: -f9 <<< "$N")"
 
   #carregar contest
   unset ALLOWLATEUSER
@@ -295,6 +296,26 @@ for ARQ in $SUBMISSIONDIR/*; do
       sed -i "s/^$TEMPO:\(.*\):$ID$/$TEMPO:\1 (Rejulgando):$ID/" $CONTESTSDIR/$CONTEST/controle/history
     fi
 
+    elif [[ "$COMANDO" == "jplag" ]]; then
+    	ACAO="$(cut -d: -f6 <<< "$N")"
+    	LINGUAGEM="$(cut -d: -f7 <<< "$N")"
+
+    	#baixar o jplag e alocar dentro da pasta do contest
+	    if [ -d "$CONTESTSDIR/$CONTEST_ID/jplag" ]; then
+  		  mkdir -p "$CONTESTSDIR/$CONTEST_ID/jplag"
+	    fi
+
+      if [ -z $(find "$CONTESTSDIR"/"$CONTEST_ID"/jplag/  -maxdepth 1 -name '*.jar' -printf 1 -quit) ]; then
+          wget  --directory-prefix=$CONTESTSDIR/$CONTEST_ID/jplag/ https://github.com/jplag/JPlag/releases/download/v3.0.0/jplag-3.0.0-jar-with-dependencies.jar &> /dev/null
+      fi
+
+    	if [[ "$ACAO" == "analisar" ]]; then
+        if [ -d "$HTMLDIR/jplag" ]; then
+      		mkdir -p "$HTMLDIR/jplag"
+        fi
+      	java -jar "$CONTESTSDIR"/"$CONTEST_ID"/jplag/*.jar -l $LINGUAGEM $CONTESTSDIR/$CONTEST/submissions/ -r "$HTMLDIR"/jplag/$CONTEST_ID/ &> /dev/null 
+  	  fi
+
   elif [[ "$COMANDO" == "submit" ]]; then
     #SITE do problema:
     SITE=${PROBS[PROBID]}
@@ -335,7 +356,7 @@ for ARQ in $SUBMISSIONDIR/*; do
       updatescore $CONTEST
     fi
 
-
+    LING="$(echo $LING | tr '[:upper:]' '[:lower:]')"
     #copiar $ARQ para o diretorio com historico de submissoes
     cp "$ARQ" "$CONTESTSDIR/$CONTEST/submissions/$ID-$LOGIN-${PROBS[PROBID+3]}.$LING"
 
